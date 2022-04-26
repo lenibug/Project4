6e1616f127dab381ea624fe29327b49c844a04ab
leniroeser <104405835+leniroeser@users.noreply.github.com>
2022-04-25 21:57:15 -0500

    Update StudentStrategy.java

--- src/spaceexplorers/strategies/StudentStrategy.java
+++ src/spaceexplorers/strategies/StudentStrategy.java
@@ -15,16 +15,28 @@ public class StudentStrategy implements IStrategy {
      * @param planetOperations Helper methods students can use to interact with the system.
      * @param eventsToExecute  Queue students will add to in order to schedule events.
      */
+    
     @Override
     public void takeTurn(List<IPlanet> planets, IPlanetOperations planetOperations, Queue<IEvent> eventsToExecute) {
         List<IVisiblePlanet> occupiedVisiblePlanets = new ArrayList<>();
         List<IVisiblePlanet> unOccupiedVisiblePlanets = new ArrayList<>();
+        List<IVisiblePlanet> neutralPlanets = new ArrayList<>();
+        List<IVisiblePlanet> ownedPlanets = new ArrayList<>();
+        List<IVisiblePlanet> enemyPlanets = new ArrayList<>():
         for (IPlanet planet : planets) {
             if (planet instanceof IVisiblePlanet && ((IVisiblePlanet) planet).getOwner() == Owner.SELF) {
                 occupiedVisiblePlanets.add((IVisiblePlanet) planet);
             } else if (planet instanceof IVisiblePlanet)
                 unOccupiedVisiblePlanets.add((IVisiblePlanet) planet);
         }
+        for(IPlanet planet : occupiedVisiblePlanets){
+            if(planet.getOwner() == Owner.NEUTRAL){
+                neutralPlanets.put(planet.getId(), planet);
+            }else if(planet.getOwner() == Owner.SELF){
+                ownedPlanets.put(planet.getId(), planet);
+            }else{
+                enemyPlanets.put(planet.getId(), planet);
+            }
     }
 
     @Override
